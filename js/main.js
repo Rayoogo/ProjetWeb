97df73fd6a260746b4e60485994545ce8930c706
Rayoogo <raphael.perricaud@efrei.net>
2026-05-10 13:51:23 +0200

    site quasi fini

--- js/main.js
+++ js/main.js
@@ -147,3 +147,40 @@ document.querySelectorAll('.nav-link').forEach(link => {
     link.classList.remove('active');
   }
 });
+
+
+// Carousel
+const slides = document.querySelector('.slides');
+const images = document.querySelectorAll('.slides img');
+
+const prevBtn = document.querySelector('.prev');
+const nextBtn = document.querySelector('.next');
+
+let index = 0;
+
+function showSlide(i){
+    if(i >= images.length){
+        index = 0;
+    }
+    else if(i < 0){
+        index = images.length - 1;
+    }
+    else{
+        index = i;
+    }
+
+    slides.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+    showSlide(index + 1);
+});
+
+prevBtn.addEventListener('click', () => {
+    showSlide(index - 1);
+});
+
+// Défilement automatique
+setInterval(() => {
+    showSlide(index + 1);
+}, 5000);
